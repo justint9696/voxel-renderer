6ce7c4cc2d8b5686d6b6e24556ff997e4cfd8a24
justint9696 <justin.tonkinson96@gmail.com>
2025-09-21 16:12:25 -0600

    feat: added dynamic window title, added time variables

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "renderer/shader.hpp"
 #include "renderer/renderer.hpp"
+#include "time.hpp"
 #include "logger.hpp"
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <cstring>
 
 static float vertices[] = {
     -0.5f, -0.5f, 0.0f,
@@ -13,9 +15,13 @@ static float vertices[] = {
 };
 
 struct State {
+    time_t now;
+    time_t last_second;
+    time_t last_tick;
+    uint32_t fps;
+    uint32_t tps;
     uint32_t vao;
     uint32_t vbo;
-    Shader shader;
 };
 
 static State g_state;
@@ -23,6 +29,7 @@ static State g_state;
 int main(int argc, char *argv[]) {
     lg::set_file("output.log");
 
+    memset(&g_state, 0, sizeof(g_state));
     renderer::init(glm::vec2(640, 480), "Game");
 
     glGenVertexArrays(1, &g_state.vao);
@@ -42,15 +49,39 @@ int main(int argc, char *argv[]) {
     renderer::shader::use("default");
 
     const Window& window = renderer::get_window();
+    Shader& shader = renderer::shader::get("default");
+
+    g_state.now = util::time::now();
+    g_state.last_second = g_state.now;
+    g_state.last_tick = g_state.now;
+    g_state.last_tick = g_state.now;
+
     while (window.is_open()) {
         if (glfwGetKey(window.handle, GLFW_KEY_Q) == GLFW_PRESS) {
             window.close();
+            break;
         }
+
+        g_state.now = util::time::now();
+        if ((g_state.now - g_state.last_second) > NS_PER_SECOND) {
+            window.set_title("Game - FPS: {} - TPS: {}",
+                             g_state.fps, g_state.tps);
+            g_state.last_second = g_state.now;
+            g_state.fps = 0;
+            g_state.tps = 0;
+        }
+
+        while ((g_state.now - g_state.last_tick) > NS_PER_TICK) {
+            g_state.last_tick += NS_PER_TICK;
+            g_state.tps++;
+        }
+
+        g_state.fps++;
 
         float a = fabs(sin(glfwGetTime()));
         float b = fabs(1 - sin(glfwGetTime()));
         float c = fabs(cos(glfwGetTime()));
-        renderer::shader::get("default").set<glm::vec4>("u_color", glm::vec4(a, b, c, 1.0f));
+        shader.set<glm::vec4>("u_color", glm::vec4(a, b, c, 1.0f));
 
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
--- src/renderer/window.hpp
+++ src/renderer/window.hpp
@@ -4,6 +4,7 @@
 #include <GLFW/glfw3.h>
 
 #include <glm/vec2.hpp>
+#include <format>
 #include <string>
 
 class Window {
@@ -13,10 +14,15 @@ public:
 
 public:
     Window() = default;
-    ~Window() {
-    }
+    ~Window() = default;
 
     Window(glm::vec2 size, const std::string& title);
+
+    template <typename... Args>
+    void set_title(const std::string& fmt, Args&&... args) const {
+        auto title = std::vformat(fmt, std::make_format_args(args...));
+        glfwSetWindowTitle(this->handle, title.c_str());
+    }
 
     inline bool is_open() const {
         return !glfwWindowShouldClose(this->handle);
